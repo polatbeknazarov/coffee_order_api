e15c5248a6e33c9d57ba07cd756e2f902a4875fe
polat <polatbekbeknazarov2003@gmail.com>
2025-01-25 01:41:13 +0500

    Add lifespan event handler with db helper dispose

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
 import uvicorn
 
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from core.config import settings
+from core.models import db_helper
 from api import router as api_router
 
-main_app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+    await db_helper.dispose()
+
+
+main_app = FastAPI(lifespan=lifespan)
 main_app.include_router(api_router, prefix=settings.api.prefix)
 
 if __name__ == "__main__":
