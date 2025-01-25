bf5cdf775971eba713bd951ff5374593815da858
polat <polatbekbeknazarov2003@gmail.com>
2025-01-25 15:58:48 +0500

    Create User model

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -1,7 +1,11 @@
 __all__ = (
     "db_helper",
     "Base",
+    "UserRole",
+    "User",
 )
 
 from .db_helper import db_helper
 from .base import Base
+from .enum import UserRole
+from .user import User
---
+++ app/core/models/user.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Enum
+
+from .base import Base
+from .enum import UserRole
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    username: Mapped[str] = mapped_column(unique=True, nullable=False, index=True)
+    email: Mapped[str] = mapped_column(unique=True, nullable=False, index=True)
+    hashed_password: Mapped[bytes] = mapped_column(nullable=False)
+
+    role: Mapped[UserRole] = mapped_column(
+        Enum(UserRole),
+        default=UserRole.READER,
+        nullable=False,
+    )
+
+    is_active: Mapped[bool] = mapped_column(default=True)
