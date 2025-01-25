4bebb64f195e30540854445c3352ffe4fc910a87
polat <polatbekbeknazarov2003@gmail.com>
2025-01-26 00:09:43 +0500

    Update base model

--- app/core/models/base.py
+++ app/core/models/base.py
@@ -1,7 +1,19 @@
+from datetime import datetime
+
+from sqlalchemy import TIMESTAMP, Integer, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
 
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP, server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP,
+        server_default=func.now(),
+        onupdate=func.now(),
+    )
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__}(id={self.id})>"
