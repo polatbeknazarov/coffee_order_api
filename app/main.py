ec33abaff06798cac507bf3492e91ee741990b48
polat <polatbekbeknazarov2003@gmail.com>
2025-01-26 00:11:39 +0500

    Add logging configs

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import logging
 import uvicorn
 
 from contextlib import asynccontextmanager
@@ -6,6 +7,13 @@ from fastapi import FastAPI
 from core.config import settings
 from core.models import db_helper
 from api import router as api_router
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S",
+    format="[%(asctime)s.%(msecs)03d] %(module)20s:%(lineno)-3d %(levelname)-7s - %(message)s",
+)
 
 
 @asynccontextmanager
