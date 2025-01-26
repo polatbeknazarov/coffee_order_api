13f6470f3569c1b23b7f125f4d111c432b740254
polat <polatbekbeknazarov2003@gmail.com>
2025-01-27 01:37:54 +0500

    Add refresh token generation to login endpoint

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 
 from core.config import settings
 from core.schemas import UserRead, TokenSchema
-from auth.utils import create_access_token
+from auth.utils import create_access_token, create_refresh_token
 from auth.validations import validate_create_user, validate_auth_user
 
 auth_router = APIRouter(prefix=settings.api.v1.auth, tags=["Auth"])
@@ -16,4 +16,5 @@ async def register(user: UserRead = Depends(validate_create_user)):
 @auth_router.post("/login", response_model=TokenSchema)
 async def login(user: UserRead = Depends(validate_auth_user)):
     access_token = create_access_token(user=user)
-    return TokenSchema(access_token=access_token)
+    refresh_token = create_refresh_token(user=user)
+    return TokenSchema(access_token=access_token, refresh_token=refresh_token)
