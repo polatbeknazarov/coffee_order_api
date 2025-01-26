a3e0fcb6a63f9c1903d82428a3f68fae22daea6a
polat <polatbekbeknazarov2003@gmail.com>
2025-01-26 23:34:24 +0500

    Add users route to fetch the current authenticated user

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter
 
 from core.config import settings
 from .auth import auth_router
+from .users import users_router
 
 api_v1_router = APIRouter(prefix=settings.api.v1.prefix)
 api_v1_router.include_router(auth_router)
+api_v1_router.include_router(users_router)
---
+++ app/api/v1/users.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, Depends
+
+from core.config import settings
+from core.schemas import UserRead
+from auth.dependencies import get_current_user
+
+users_router = APIRouter(prefix=settings.api.v1.users, tags=["Users"])
+
+
+@users_router.get("/me", response_model=UserRead)
+async def get_current_auth_user(
+    user: UserRead = Depends(get_current_user),
+):
+    return user
