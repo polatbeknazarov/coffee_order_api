c550d951fd80dadbe8191da329e751ceb55df469
polat <polatbekbeknazarov2003@gmail.com>
2025-01-27 02:42:19 +0500

    Add HTTP Bearer authentication dependency to auth router

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -4,8 +4,13 @@ from core.config import settings
 from core.schemas import UserRead, TokenSchema
 from auth.utils import create_access_token, create_refresh_token
 from auth.validations import validate_create_user, validate_auth_user
+from auth.dependencies import get_user_from_refresh_token, http_bearer
 
-auth_router = APIRouter(prefix=settings.api.v1.auth, tags=["Auth"])
+auth_router = APIRouter(
+    prefix=settings.api.v1.auth,
+    tags=["Auth"],
+    dependencies=[Depends(http_bearer)],
+)
 
 
 @auth_router.post("/register", response_model=UserRead)
