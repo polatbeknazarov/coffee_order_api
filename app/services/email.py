51d44e768cfb84158558c3106d0ffe722b0e2482
polat <polatbekbeknazarov2003@gmail.com>
2025-02-14 19:25:12 +0500

    Add send_email function using aiosmtplib

---
+++ app/services/email.py
@@ -0,0 +1,27 @@
+import logging
+import aiosmtplib
+
+from email.message import EmailMessage
+
+from core.config import settings
+
+
+async def send_email(to_email: str, subject: str, body: str):
+    msg = EmailMessage()
+    msg["From"] = settings.smtp.user
+    msg["To"] = to_email
+    msg["Subject"] = subject
+    msg.set_content(body)
+
+    try:
+        await aiosmtplib.send(
+            msg,
+            hostname=settings.smtp.host,
+            port=settings.smtp.port,
+            username=settings.smtp.user,
+            password=settings.smtp.password,
+            start_tls=True,
+        )
+        logging.info("Email successfully sent to %s", to_email)
+    except Exception as e:
+        logging.error("Error sending email: %s", e)
