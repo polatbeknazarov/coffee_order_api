a3a6de361cde9a70cfe422e09dfbff44d6fa7073
polat <polatbekbeknazarov2003@gmail.com>
2025-02-15 18:04:50 +0500

    Add verification_token_expire_minutes to AuthJWT config

--- app/core/config.py
+++ app/core/config.py
@@ -28,6 +28,7 @@ class AuthJWT(BaseModel):
     algorithm: str = "RS256"
     access_token_expire_minutes: int = 15
     refresh_token_expire_minutes: int = 60 * 24 * 30
+    verification_token_expire_minutes: int = 20
 
 
 class DatabaseConfig(BaseModel):
