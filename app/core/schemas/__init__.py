e42b791dd175064c23f725376ec991fc013ab1c3
polat <polatbekbeknazarov2003@gmail.com>
2025-02-17 03:01:33 +0500

    Add product filter schema

--- app/core/schemas/__init__.py
+++ app/core/schemas/__init__.py
@@ -15,10 +15,11 @@ __all__ = (
     "ProductCreate",
     "ProductRead",
     "ProductUpdate",
+    "ProductFilter",
 )
 
 from .auth import LoginRequest, RegisterRequest
 from .token import TokenSchema
 from .user import UserBase, UserRead, UserCreate, UserUpdate, UserAdminCreate
 from .category import CategoryBase, CategoryCreate, CategoryRead, CategoryUpdate
-from .product import ProductBase, ProductCreate, ProductRead, ProductUpdate
+from .product import ProductBase, ProductCreate, ProductRead, ProductUpdate, ProductFilter
--- app/core/schemas/product.py
+++ app/core/schemas/product.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel
+from fastapi_filter.contrib.sqlalchemy import Filter
 
 from .category import CategoryRead
+from core.models import Product
 
 
 class ProductBase(BaseModel):
@@ -21,3 +23,15 @@ class ProductRead(ProductBase):
 
 class ProductUpdate(ProductBase):
     is_available: bool
+
+
+class ProductFilter(Filter):
+    name__in: list | None = None
+    price__gte: float | None = None
+    price__lte: float | None = None
+
+    class Constants(Filter.Constants):
+        model = Product
+
+    class Config:
+        allow_population_by_field_name = True
