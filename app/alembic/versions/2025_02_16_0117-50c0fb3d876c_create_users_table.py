6e93794a07df31cc66fd7c6b89a18ecd6b1bdf33
polat <polatbekbeknazarov2003@gmail.com>
2025-02-17 02:37:29 +0500

    Add create_users_table migration

---
+++ app/alembic/versions/2025_02_16_0117-50c0fb3d876c_create_users_table.py
@@ -0,0 +1,59 @@
+"""Create users table
+
+Revision ID: 50c0fb3d876c
+Revises: 
+Create Date: 2025-02-16 01:17:30.537369
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "50c0fb3d876c"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "users",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("username", sa.String(), nullable=False),
+        sa.Column("email", sa.String(), nullable=False),
+        sa.Column("hashed_password", sa.LargeBinary(), nullable=False),
+        sa.Column(
+            "role", sa.Enum("USER", "ADMIN", name="userrole"), nullable=False
+        ),
+        sa.Column("is_active", sa.Boolean(), nullable=False),
+        sa.Column("is_verified", sa.Boolean(), nullable=False),
+        sa.Column(
+            "created_at",
+            sa.TIMESTAMP(),
+            server_default=sa.text("now()"),
+            nullable=False,
+        ),
+        sa.Column(
+            "updated_at",
+            sa.TIMESTAMP(),
+            server_default=sa.text("now()"),
+            nullable=False,
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_index(op.f("ix_users_email"), "users", ["email"], unique=True)
+    op.create_index(op.f("ix_users_id"), "users", ["id"], unique=False)
+    op.create_index(
+        op.f("ix_users_username"), "users", ["username"], unique=True
+    )
+
+
+def downgrade() -> None:
+    op.drop_index(op.f("ix_users_username"), table_name="users")
+    op.drop_index(op.f("ix_users_id"), table_name="users")
+    op.drop_index(op.f("ix_users_email"), table_name="users")
+    op.drop_table("users")
