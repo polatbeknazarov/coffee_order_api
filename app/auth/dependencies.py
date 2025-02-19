8b09c53aed5609accae905283d353afa76b26db2
polat <polatbekbeknazarov2003@gmail.com>
2025-02-19 23:18:29 +0500

    Add require_role function

--- app/auth/dependencies.py
+++ app/auth/dependencies.py
@@ -8,11 +8,13 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from jwt.exceptions import InvalidTokenError
 
 from core.config import settings
-from core.models import db_helper
+from core.models import db_helper, UserRole
 from core.schemas import UserRead
 from crud.users import UserDAO
 from .utils import decode_jwt
 from .validations import validate_token_type
+
+log = logging.getLogger(__name__)
 
 http_bearer = HTTPBearer(auto_error=False)
 oauth2_schema = OAuth2PasswordBearer(
@@ -24,7 +26,7 @@ async def get_token_payload(token: str = Depends(oauth2_schema)) -> dict:
     try:
         payload = decode_jwt(token=token)
     except InvalidTokenError as e:
-        logging.error("Invalid token error: %s. Token: %s", e, token)
+        log.error("Invalid token error: %s. Token: %s", e, token)
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid token error.",
@@ -84,7 +86,21 @@ async def get_current_user(user: UserRead = Depends(get_current_auth_user)):
     )
 
 
-async def get_unverified_user_by_email(email: str, session: AsyncSession = Depends(db_helper.session_getter)):
+def require_role(required_role: UserRole):
+    def role_checker(user: UserRead = Depends(get_current_user)) -> UserRead:
+        if user.role != required_role:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You don't have permission to access this resource.",
+            )
+        return user
+
+    return role_checker
+
+
+async def get_unverified_user_by_email(
+    email: str, session: AsyncSession = Depends(db_helper.session_getter)
+):
     user = await UserDAO.get_user_by_email(email=email, session=session)
 
     if not user:
