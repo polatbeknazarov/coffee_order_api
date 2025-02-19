5a158f70ae16fe5b60f4abed93959540162ce5be
polat <polatbekbeknazarov2003@gmail.com>
2025-02-20 00:17:17 +0500

    Create create_static_info endpoint

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -7,6 +7,7 @@ from .categories import categories_router
 from .products import products_router
 from .carts import carts_router
 from .orders import orders_router
+from .static_info import static_info_router
 
 api_v1_router = APIRouter(prefix=settings.api.v1.prefix)
 api_v1_router.include_router(auth_router)
@@ -15,3 +16,4 @@ api_v1_router.include_router(categories_router)
 api_v1_router.include_router(products_router)
 api_v1_router.include_router(carts_router)
 api_v1_router.include_router(orders_router)
+api_v1_router.include_router(static_info_router)
---
+++ app/api/v1/static_info.py
@@ -0,0 +1,42 @@
+from typing import List
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import IntegrityError
+
+from core.config import settings
+from core.models import db_helper, UserRole
+from core.schemas import (
+    StaticInfoRead,
+    StaticInfoCreate,
+    StaticInfoUpdate,
+    UserRead,
+)
+from auth.dependencies import http_bearer, require_role
+from crud.static_info import StaticInfoDAO
+
+static_info_router = APIRouter(
+    prefix=settings.api.v1.static_info,
+    tags=["Static Info"],
+    dependencies=[Depends(http_bearer)],
+)
+
+
+@static_info_router.post("", response_model=StaticInfoRead)
+async def create_static_info(
+    request: StaticInfoCreate,
+    admin: UserRead = Depends(require_role(UserRole.ADMIN)),
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    try:
+        static_info = await StaticInfoDAO.create(
+            validated_values=request,
+            session=session,
+        )
+    except IntegrityError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f'Static info with key "{request.key}" already exists.',
+        )
+
+    return static_info
