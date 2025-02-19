59c591e7e5426d601e002b6048807f2ef4e17560
polat <polatbekbeknazarov2003@gmail.com>
2025-02-19 23:28:43 +0500

    Create OrderDAO

---
+++ app/crud/orders.py
@@ -0,0 +1,52 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from .base import BaseDAO
+from .carts import CartDAO
+from .products import ProductDAO
+from core.models import Order, OrderItem
+from core.schemas import OrderBase
+
+
+class OrderDAO(BaseDAO[Order, OrderBase]):
+    model = Order
+
+    @classmethod
+    async def create_order(
+        cls,
+        user_id: int,
+        session: AsyncSession,
+    ) -> Order:
+        cart_items = await CartDAO.get_carts_by_user_id(
+            user_id=user_id, session=session
+        )
+        if not cart_items:
+            raise ValueError("Cart items not found")
+
+        total_price = 0
+
+        for item in cart_items:
+            product = await ProductDAO.get_by_id(
+                model_id=item.product_id,
+                session=session,
+            )
+            if not product or not product.is_available:
+                raise ValueError(f"Product {item.product_id} unavailable.")
+
+            total_price += item.quantity * product.price
+
+        order = Order(user_id=user_id, total_price=total_price)
+        session.add(order)
+        await session.flush()
+        await session.commit()
+
+        for item in cart_items:
+            order_item = OrderItem(
+                order_id=order.id,
+                product_id=item.product_id,
+                quantity=item.quantity,
+            )
+            session.add(order_item)
+
+        await session.commit()
+
+        return order
