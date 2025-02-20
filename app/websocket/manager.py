a860cc72a2a9910f6ce68c1be14965094f27b569
polat <polatbekbeknazarov2003@gmail.com>
2025-02-20 18:43:41 +0500

    Add websocket connection manager

---
+++ app/websocket/manager.py
@@ -0,0 +1,35 @@
+import json
+
+from typing import Dict, List
+
+import pydantic
+from fastapi import WebSocket
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: Dict[int, List[WebSocket]] = {}
+
+    async def connect(self, websocket: WebSocket, user_id: int):
+        await websocket.accept()
+        if user_id not in self.active_connections:
+            self.active_connections[user_id] = []
+        self.active_connections[user_id].append(websocket)
+
+    def disconnect(self, websocket: WebSocket, user_id: int):
+        if user_id in self.active_connections:
+            self.active_connections[user_id].remove(websocket)
+            if not self.active_connections[user_id]:
+                del self.active_connections[user_id]
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, user_id: int, message: pydantic.BaseModel):
+        if user_id in self.active_connections:
+            for connection in self.active_connections[user_id]:
+                data = message.json()
+                await connection.send_json(json.loads(data))
+
+
+manager = ConnectionManager()
